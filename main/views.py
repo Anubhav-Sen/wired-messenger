4274138044bac242491776d58b2a4f2cbb0fdc13
Anubhav Sen <anubhav.sen@code.berlin>
2022-10-25 15:00:24 +0200

    created basic templates and static files and configured settings to fetch files from respective directories

--- main/views.py
+++ main/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("hello world!")
+    context = {}
+    return render(request, "index.html", context)
