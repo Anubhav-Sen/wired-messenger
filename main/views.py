fdd1334b14e7c1e6a19d2a58aa83a1aaec7a26b3
Anubhav Sen <anubhav.sen@code.berlin>
2022-10-27 18:19:13 +0200

    created login view, added login template, and added login-signup css

--- main/views.py
+++ main/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index(request):
     context = {}
     return render(request, "index.html", context)
+
+def login(request):
+    context = {}
+    return render(request, "login.html", context)
